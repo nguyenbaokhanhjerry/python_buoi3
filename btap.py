7d34996029a437f43735b63b4e2c27a998d7458b
nguyenbaokhanhjerry <khanhbaonguyen76@gmail.com>
2026-05-24 09:33:46 +0700

    Update btap.py

--- btap.py
+++ btap.py
@@ -1,6 +1,6 @@
 order_list = ["GE001", "GE002", "GE003"]
 while True:
-    print("\n===== HỆ THỐNG QUẢN LÝ ĐƠN HÀNG GRAB EXPRESS =====")
+    print(" --- HỆ THỐNG QUẢN LÝ ĐƠN HÀNG GRAB EXPRESS --- ")
     print("1. Hiển thị danh sách đơn hàng")
     print("2. Thêm đơn hàng mới")
     print("3. Xóa đơn hàng theo mã")
@@ -8,7 +8,7 @@ while True:
     choice = input("Nhập lựa chọn của bạn: ").strip()
     if choice == "1":
         if order_list == []:
-            print("Danh sách đơn hàng đang trống.")
+            print("Danh sách đơn hàng đang trống")
         else:
             print("Danh sách đơn hàng:")
             stt = 1
@@ -19,7 +19,7 @@ while True:
         new_order = input("Nhập mã đơn hàng mới: ")
         new_order = new_order.strip().upper()
         if new_order == "":
-            print("Mã đơn hàng không được để trống.")
+            print("Mã đơn hàng không được để trống")
         else:
             order_list.append(new_order)
             print("Thêm đơn hàng thành công!")
@@ -33,7 +33,7 @@ while True:
         else:
             print("Không tìm thấy mã đơn hàng cần xóa!")
     elif choice == "4":
-        print("Thoát chương trình.")
+        print("Thoát chương trình")
         break
     else:
         print("Lựa chọn không hợp lệ, vui lòng nhập lại!")
